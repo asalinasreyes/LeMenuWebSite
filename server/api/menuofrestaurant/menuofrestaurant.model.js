2fd447e2da0741288f35d0f77678576adc594b1b
alexis.salinas.reyes <alexis.salinas.reyes@gmail.com>
2015-08-16 01:32:44 -0400

    se agrega lookfeel a login y signup, se agregar menu agrega files en arreglo, lista informacion de cant de archivos al menu

--- server/api/menuofrestaurant/menuofrestaurant.model.js
+++ server/api/menuofrestaurant/menuofrestaurant.model.js
@@ -1,15 +1,33 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+	Schema = mongoose.Schema;
+
+
+var FileUploadSchema = new Schema({
+	public_id: String,
+	filename: String,
+	url: String
+});
 
 var MenuofrestaurantSchema = new Schema({
-  name: String,
-  active: Boolean,
-  language:[String],
-  restaurantid: { type: Schema.ObjectId, ref: 'RestaurantSchema' },
-  userid: { type: Schema.ObjectId, ref: 'UserSchema' },
-  created_at    : { type: Date, required: true, default: Date.now }
+	name: String,
+	active: Boolean,
+	language: [String],
+	files: [FileUploadSchema],
+	restaurantid: {
+		type: Schema.ObjectId,
+		ref: 'RestaurantSchema'
+	},
+	userid: {
+		type: Schema.ObjectId,
+		ref: 'UserSchema'
+	},
+	created_at: {
+		type: Date,
+		required: true,
+		default: Date.now
+	}
 });
 
 module.exports = mongoose.model('Menuofrestaurant', MenuofrestaurantSchema);
