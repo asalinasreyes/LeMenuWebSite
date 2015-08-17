1bae29c7dc1884675a239c961865a42721ee03d3
alexis.salinas.reyes <alexis.salinas.reyes@gmail.com>
2015-08-17 14:13:43 -0400

    correccion de estilos, se edit, crea y modifica menu

--- client/app/owner/mymenu/mymenu.js
+++ client/app/owner/mymenu/mymenu.js
@@ -7,6 +7,10 @@ angular.module('leMeNuApp')
                 url: '/new',
                 templateUrl: 'app/owner/mymenu/newmenu/newmenu.html'
             })
+            .state('owner.mymenu.edit', {
+                url: '/edit',
+                templateUrl: 'app/owner/mymenu/editmenu/editmenu.html'
+            })
             .state('owner.mymenu.listmenu', {
                 url: '/list',
                 templateUrl: 'app/owner/mymenu/listmenu/listmenu.html'
--- server/api/menuofrestaurant/menuofrestaurant.controller.js
+++ server/api/menuofrestaurant/menuofrestaurant.controller.js
@@ -72,7 +72,7 @@ exports.update = function(req, res) {
         if (!menuofrestaurant) {
             return res.send(404);
         }
-        var updated = _.merge(menuofrestaurant, req.body);
+        var updated = _.extend(menuofrestaurant, req.body);
         updated.save(function(err) {
             if (err) {
                 return handleError(res, err);
