c0df140f9f72c105193e4064becff8a2eb23a6e6
alexis.salinas.reyes <alexis.salinas.reyes@gmail.com>
2015-08-19 22:26:27 -0400

    se agrega info invoice, se agrega traduccion al app.js

--- client/app/owner/payment/payment.controller.js
+++ client/app/owner/payment/payment.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('leMeNuApp')
-    .controller('PaymentownerCtrl', function($scope) {
-        $scope.PriceSubTotal = 12;
-    });
+	.controller('PaymentownerCtrl', function($scope, $state, $modal, mySharedService) {
+		$scope.PriceSubTotal = 12;
+
+		$scope.RestaurantSelectedInfo = mySharedService.message;
+		
+		$scope.paymentMenuSeleted = $scope.RestaurantSelectedInfo.ItemMenuSelected;
+		var numberFiles = $scope.paymentMenuSeleted.files.length;
+
+			$scope.infoInformation  = {
+			date: new Date(),
+			price: 10,
+			countpage: numberFiles,
+			priceTotal: numberFiles * 10
+		};
+	
+		
+	});
