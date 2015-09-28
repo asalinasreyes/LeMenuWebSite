6675a39ae50181da0a1fb1fd90d68937ea20b463
alexis.salinas.reyes <alexis.salinas.reyes@gmail.com>
2015-09-28 10:55:00 -0400

    se quita objeto obsoleto

--- server/routes.js
+++ server/routes.js
@@ -21,8 +21,6 @@ module.exports = function(app) {
     app.use('/api/photomenus', require('./api/photomenu'));
 
     app.use('/api/movil', require('./api/movil'));    
-
-    app.use('/api/things', require('./api/thing'));
     app.use('/api/users', require('./api/user'));
 
     app.use('/auth', require('./auth'));
